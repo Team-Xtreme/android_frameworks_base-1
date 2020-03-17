Havoc-OS/android_frameworks_base

base: Add ambient pulse notification [1/2]
@SKULSHADY: Clean it up a bit

Change-Id: Ie4f1730bb15d0cd4db7ceb3a0281c114707d3789

packages/SystemUI/src/com/android/systemui/havoc/NotificationLightsView.java
@@ -1,0 +1,115 @@
+/*
+* Copyright (C) 2019 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.android.systemui.havoc;
+
+import android.animation.ValueAnimator;
+import android.animation.ValueAnimator.AnimatorUpdateListener;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+
+import com.android.systemui.R;
+
+public class NotificationLightsView extends RelativeLayout {
+
+    private View mNotificationAnimView;
+    private ValueAnimator mLightAnimator;
+    private boolean mPulsing;
+
+    public NotificationLightsView(Context context) {
+        this(context, null);
+    }
+
+    public NotificationLightsView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public NotificationLightsView(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public NotificationLightsView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        Log.e("NotificationLightsView", "new");
+    }
+
+    private Runnable mLightUpdate = new Runnable() {
+        @Override
+        public void run() {
+            Log.e("NotificationLightsView", "run");
+            animateNotification();
+        }
+    };
+
+    public void setPulsing(boolean pulsing) {
+        if (mPulsing == pulsing) {
+            return;
+        }
+        mPulsing = pulsing;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        Log.e("NotificationLightsView", "draw");
+    }
+
+    public void animateNotification() {
+        int color = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.PULSE_AMBIENT_LIGHT_COLOR, 0xFF3980FF,
+                UserHandle.USER_CURRENT);
+        StringBuilder sb = new StringBuilder();
+        sb.append("animateNotification color ");
+        sb.append(Integer.toHexString(color));
+        Log.e("NotificationLightsView", sb.toString());
+        ImageView leftView = (ImageView) findViewById(R.id.notification_animation_left);
+        ImageView rightView = (ImageView) findViewById(R.id.notification_animation_right);
+        leftView.setColorFilter(color);
+        rightView.setColorFilter(color);
+        mLightAnimator = ValueAnimator.ofFloat(new float[]{0.0f, 2.0f});
+        mLightAnimator.setDuration(2000);
+        mLightAnimator.addUpdateListener(new AnimatorUpdateListener() {
+            public void onAnimationUpdate(ValueAnimator animation) {
+                Log.e("NotificationLightsView", "onAnimationUpdate");
+                float progress = ((Float) animation.getAnimatedValue()).floatValue();
+                leftView.setScaleY(progress);
+                rightView.setScaleY(progress);
+                float alpha = 1.0f;
+                if (progress <= 0.3f) {
+                    alpha = progress / 0.3f;
+                } else if (progress >= 1.0f) {
+                    alpha = 2.0f - progress;
+                }
+                leftView.setAlpha(alpha);
+                rightView.setAlpha(alpha);
+            }
+        });
+        Log.e("NotificationLightsView", "start");
+        mLightAnimator.start();
+    }
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -75,6 +75,7 @@ import com.android.systemui.R;
 import com.android.systemui.classifier.FalsingManagerFactory;
 import com.android.systemui.fragments.FragmentHostManager;
 import com.android.systemui.fragments.FragmentHostManager.FragmentListener;
+import com.android.systemui.havoc.NotificationLightsView;
 import com.android.systemui.plugins.FalsingManager;
 import com.android.systemui.plugins.qs.QS;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
@@ -294,6 +295,7 @@ public class NotificationPanelView extends PanelView implements
     private FalsingManager mFalsingManager;
     private String mLastCameraLaunchSource = KeyguardBottomAreaView.CAMERA_LAUNCH_SOURCE_AFFORDANCE;
     private LockPatternUtils mLockPatternUtils;
+    private NotificationLightsView mPulseLightsView;
 
     private boolean mStatusBarShownOnSecureKeyguard;
 
@@ -504,6 +506,7 @@ public class NotificationPanelView extends PanelView implements
         mKeyguardBottomArea = findViewById(R.id.keyguard_bottom_area);
         mQsNavbarScrim = findViewById(R.id.qs_navbar_scrim);
         mLastOrientation = getResources().getConfiguration().orientation;
+        mPulseLightsView = (NotificationLightsView) findViewById(R.id.lights_container);
 
         initBottomArea();
 
@@ -3353,6 +3356,9 @@ public class NotificationPanelView extends PanelView implements
         DozeParameters dozeParameters = DozeParameters.getInstance(mContext);
         final boolean animatePulse = !dozeParameters.getDisplayNeedsBlanking()
                 && dozeParameters.getAlwaysOn();
+        boolean pulseLights = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.PULSE_AMBIENT_LIGHT, 0, UserHandle.USER_CURRENT) != 0;
+
         if (animatePulse) {
             mAnimateNextPositionUpdate = true;
         }
@@ -3360,6 +3366,13 @@ public class NotificationPanelView extends PanelView implements
         // The height callback will take care of pushing the clock to the right position.
         if (!mPulsing && !mDozing) {
             mAnimateNextPositionUpdate = false;
+        }
+        if ((mPulseLightsView != null) && pulseLights) {
+            mPulseLightsView.setVisibility(mPulsing ? View.VISIBLE : View.GONE);
+            if (mPulsing) {
+                mPulseLightsView.animateNotification();
+                mPulseLightsView.setPulsing(pulsing);
+            }
         }
         mNotificationStackScroller.setPulsing(pulsing, animatePulse);
         mKeyguardStatusView.setPulsing(pulsing);

Team-Xtreme/android_frameworks_base-1

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/crdroid/NotificationLightsView.java
@@ -1,0 +1,115 @@
+/*
+* Copyright (C) 2019 crDroid Android Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.android.systemui.crdroid;
+
+import android.animation.ValueAnimator;
+import android.animation.ValueAnimator.AnimatorUpdateListener;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+
+import com.android.systemui.R;
+
+public class NotificationLightsView extends RelativeLayout {
+
+    private View mNotificationAnimView;
+    private ValueAnimator mLightAnimator;
+    private boolean mPulsing;
+
+    public NotificationLightsView(Context context) {
+        this(context, null);
+    }
+
+    public NotificationLightsView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public NotificationLightsView(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public NotificationLightsView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        Log.e("NotificationLightsView", "new");
+    }
+
+    private Runnable mLightUpdate = new Runnable() {
+        @Override
+        public void run() {
+            Log.e("NotificationLightsView", "run");
+            animateNotification();
+        }
+    };
+
+    public void setPulsing(boolean pulsing) {
+        if (mPulsing == pulsing) {
+            return;
+        }
+        mPulsing = pulsing;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        Log.e("NotificationLightsView", "draw");
+    }
+
+    public void animateNotification() {
+        int color = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.PULSE_AMBIENT_LIGHT_COLOR, 0xFF3980FF,
+                UserHandle.USER_CURRENT);
+        StringBuilder sb = new StringBuilder();
+        sb.append("animateNotification color ");
+        sb.append(Integer.toHexString(color));
+        Log.e("NotificationLightsView", sb.toString());
+        ImageView leftView = (ImageView) findViewById(R.id.notification_animation_left);
+        ImageView rightView = (ImageView) findViewById(R.id.notification_animation_right);
+        leftView.setColorFilter(color);
+        rightView.setColorFilter(color);
+        mLightAnimator = ValueAnimator.ofFloat(new float[]{0.0f, 2.0f});
+        mLightAnimator.setDuration(2000);
+        mLightAnimator.addUpdateListener(new AnimatorUpdateListener() {
+            public void onAnimationUpdate(ValueAnimator animation) {
+                Log.e("NotificationLightsView", "onAnimationUpdate");
+                float progress = ((Float) animation.getAnimatedValue()).floatValue();
+                leftView.setScaleY(progress);
+                rightView.setScaleY(progress);
+                float alpha = 1.0f;
+                if (progress <= 0.3f) {
+                    alpha = progress / 0.3f;
+                } else if (progress >= 1.0f) {
+                    alpha = 2.0f - progress;
+                }
+                leftView.setAlpha(alpha);
+                rightView.setAlpha(alpha);
+            }
+        });
+        Log.e("NotificationLightsView", "start");
+        mLightAnimator.start();
+    }
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -67,6 +67,7 @@ import com.android.systemui.DejankUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.Interpolators;
 import com.android.systemui.R;
+import com.android.systemui.crdroid.NotificationLightsView;
 import com.android.systemui.fragments.FragmentHostManager;
 import com.android.systemui.fragments.FragmentHostManager.FragmentListener;
 import com.android.systemui.plugins.FalsingManager;
@@ -337,6 +338,7 @@ public class NotificationPanelView extends PanelView implements
     private boolean mAffordanceHasPreview;
     private FalsingManager mFalsingManager;
     private String mLastCameraLaunchSource = KeyguardBottomAreaView.CAMERA_LAUNCH_SOURCE_AFFORDANCE;
+    private NotificationLightsView mPulseLightsView;
 
     private Runnable mHeadsUpExistenceChangedRunnable = new Runnable() {
         @Override
@@ -541,6 +543,7 @@ public class NotificationPanelView extends PanelView implements
         mKeyguardBottomArea = findViewById(R.id.keyguard_bottom_area);
         mQsNavbarScrim = findViewById(R.id.qs_navbar_scrim);
         mLastOrientation = getResources().getConfiguration().orientation;
+        mPulseLightsView = (NotificationLightsView) findViewById(R.id.lights_container);
 
         initBottomArea();
 
@@ -3369,6 +3372,9 @@ public class NotificationPanelView extends PanelView implements
         DozeParameters dozeParameters = DozeParameters.getInstance(mContext);
         final boolean animatePulse = !dozeParameters.getDisplayNeedsBlanking()
                 && dozeParameters.getAlwaysOn();
+        boolean pulseLights = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.PULSE_AMBIENT_LIGHT, 0, UserHandle.USER_CURRENT) != 0;
+
         if (animatePulse) {
             mAnimateNextPositionUpdate = true;
         }
@@ -3376,6 +3382,13 @@ public class NotificationPanelView extends PanelView implements
         // The height callback will take care of pushing the clock to the right position.
         if (!mPulsing && !mDozing) {
             mAnimateNextPositionUpdate = false;
+        }
+        if ((mPulseLightsView != null) && pulseLights) {
+            mPulseLightsView.setVisibility(mPulsing ? View.VISIBLE : View.GONE);
+            if (mPulsing) {
+                mPulseLightsView.animateNotification();
+                mPulseLightsView.setPulsing(pulsing);
+            }
         }
         mNotificationStackScroller.setPulsing(pulsing, animatePulse);
         mKeyguardStatusView.setPulsing(pulsing);