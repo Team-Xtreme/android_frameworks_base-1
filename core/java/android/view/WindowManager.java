20298377ee68ebc8c4912dc2635d5d79dcdd482f
Rpingat <pingatravi@gmail.com>
2020-03-21 13:33:28 +0530

    Revert "fwb: Slim recents Q"
    This reverts commit 21492b9402e24cb5e3094880bb6e86997d40cd43.

--- core/java/android/view/WindowManager.java
+++ core/java/android/view/WindowManager.java
@@ -709,9 +709,7 @@ public interface WindowManager extends ViewManager {
                 @ViewDebug.IntToString(from = TYPE_SCREENSHOT,
                         to = "SCREENSHOT"),
                 @ViewDebug.IntToString(from = TYPE_APPLICATION_OVERLAY,
-                        to = "APPLICATION_OVERLAY"),
-                @ViewDebug.IntToString(from = TYPE_SLIM_RECENTS,
-                        to = "TYPE_SLIM_RECENTS")
+                        to = "APPLICATION_OVERLAY")
         })
         public int type;
 
@@ -1117,13 +1115,6 @@ public interface WindowManager extends ViewManager {
          * @hide
          */
         public static final int TYPE_SYSTEM_LONGSHOT = FIRST_SYSTEM_WINDOW + 39;
-
-        /**
-         * Window type: panel that slides out from the status bar
-         * In multiuser systems shows on all users' windows.
-         * @hide
-         */
-        public static final int TYPE_SLIM_RECENTS = FIRST_SYSTEM_WINDOW+39;
 
         /**
          * End of types of system windows.
